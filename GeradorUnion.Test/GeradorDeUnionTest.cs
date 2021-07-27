13b9a171df3bf18b1b196f62f709655d5f25d17d
Bruno Ravanhani <bruno.ravanhanif@gmail.com>
2021-07-27 19:07:10 -0400

    Commit Inicial

---
+++ GeradorUnion.Test/GeradorDeUnionTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeradorUnion.Test
+{
+    [TestClass]
+    public class GeradorDeUnionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ GeradorUnion/GeradorDeUnion.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GeradorUnion
+{
+    public class GeradorDeUnion
+    {
+    }
+}
