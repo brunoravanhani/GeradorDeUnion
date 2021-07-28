d4c870d2d81429bd116b45f5ffb062e5f7dbc64a
Bruno Ravanhani <bruno.ravanhanif@gmail.com>
2021-07-27 23:35:36 -0400

    Adicionar primeira versão do algoritmo

--- GeradorUnion.Test/GeradorDeUnionTest.cs
+++ GeradorUnion.Test/GeradorDeUnionTest.cs
@@ -6,8 +6,24 @@ namespace GeradorUnion.Test
     public class GeradorDeUnionTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldValidateGerador()
         {
+            var caracteristicas = new string[]
+            {
+                "teste",
+                "teste2",
+                "teste3"
+            };
+
+            var gerador = new GeradorDeUnion();
+
+            var result = gerador.Gerar(caracteristicas);
+
+            var expected = @"Select * from Tabela where caracteristica = 'teste' UNION Select * from Tabela where caracteristica = 'teste2' UNION Select * from Tabela where caracteristica = 'teste3'";
+
+            Assert.AreEqual(
+                expected
+                , result);
         }
     }
 }
--- GeradorUnion/GeradorDeUnion.cs
+++ GeradorUnion/GeradorDeUnion.cs
@@ -1,8 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace GeradorUnion
 {
     public class GeradorDeUnion
     {
+
+        public string Gerar(string[] caracteristicas)
+        {
+            var sb = new StringBuilder();
+            var count = caracteristicas.Length;
+            for (var i = 0; i < count; i++)
+            {
+                sb.Append("Select * from Tabela where caracteristica = '");
+                sb.Append(caracteristicas[i]);
+                sb.Append('\'');
+
+                if (i < count - 1)
+                {
+                    sb.Append(" UNION ");
+
+                }
+            }
+            return sb.ToString();
+        }
     }
 }
